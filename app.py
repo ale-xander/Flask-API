2022fe1e44348148b7cf036480ccfb699b3a786e
alexiliev <iliev.alex@outlook.com>
2020-03-04 12:27:41 -0800

    adding jwt

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 api = Api(app)
 app.secret_key = 'alex'
 
+#create /auth endpoint which returns a JWT token. can send to next request we send
+jwt = JWT(app, authenticate, identity) #use jwt in app and authenticate/identity from security.py
+
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         #first item found by filter function. None is in case it didn't find anything
         item = next(filter(lambda x: x['name'] == name, items), None) 
