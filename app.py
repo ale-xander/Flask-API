8a294be99a79199247d071eca639b67016c0d3a8
alexiliev <iliev.alex@outlook.com>
2020-03-04 13:02:57 -0800

    adding parser to make sure only the price can be updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -38,7 +38,18 @@ class Item(Resource):
         return {'message': 'item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        #run arguments thru parser and see what arguments match in the parser
+        parser = reqparse.RequestParser()
+        #disregard anything other than price (such as name). and price is required
+        parser.add_argument(
+            'price',
+            type=float,
+            required=True,
+            help='this field cannnot be left blank'
+        )
+
+        data = parser.parse_args()
+        
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
