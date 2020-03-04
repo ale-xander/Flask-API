121438e55a65dcfb1f8663163794f68d8cdb450c
alexiliev <iliev.alex@outlook.com>
2020-03-04 11:47:08 -0800

    refactoring with lambda functions

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-            return {'item': None}, 404
+        #first item found by filter function. None is in case it didn't find anything
+        item = next(filter(lambda x: x['name'] == name, items), None) 
+        #now return item or None
+        return {'item': item}, 200 if item else 404
     
     def post(self, name):
+        #if item found and it is not None
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': 'this item already exists'}, 400
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
